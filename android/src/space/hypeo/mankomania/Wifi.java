0b7efc00eb681444c1f89682710984627844b6d6
Christian Motz <christian.motz@gmx.at>
2018-04-16 20:07:16 +0200

    method initialWifiLayoutComponents()

--- android/src/space/hypeo/mankomania/Wifi.java
+++ android/src/space/hypeo/mankomania/Wifi.java
@@ -2,12 +2,32 @@ package space.hypeo.mankomania;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class Wifi extends AppCompatActivity {
+
+    Button btnOnOff, btnDiscover, btnSend;
+    ListView listView;
+    TextView read_msg_box, connectionStatus;
+    EditText writeMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi);
+        initialWifiLayoutComponents();
+    }
+
+    private void initialWifiLayoutComponents() {
+        btnOnOff = (Button) findViewById(R.id.onOff);
+        btnDiscover = (Button) findViewById(R.id.discover);
+        btnSend = (Button) findViewById(R.id.sendButton);
+        listView = (ListView) findViewById(R.id.peerListView);
+        read_msg_box = (TextView) findViewById(R.id.readMsg);
+        connectionStatus = (TextView) findViewById(R.id.connectionStatus);
+        writeMsg = (EditText) findViewById(R.id.writeMsg);
     }
 }
