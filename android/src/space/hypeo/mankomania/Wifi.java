a700244925f4b66c6e530c3cdae4dbb31e68420e
Christian Motz <christian.motz@gmx.at>
2018-04-16 19:58:31 +0200

    init wifi activity

---
+++ android/src/space/hypeo/mankomania/Wifi.java
@@ -0,0 +1,13 @@
+package space.hypeo.mankomania;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Wifi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wifi);
+    }
+}
