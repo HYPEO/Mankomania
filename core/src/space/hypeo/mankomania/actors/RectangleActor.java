4643ddf4e758a387738a811660204f131dff9580
Marc Pichler (pichlermarc) <marc.pichler@edu.htl-villach.at>
2018-04-26 20:22:24 +0200

    added banknote sprite, added pink background to splash screen, added RectangleActor class to be able to render the background.

---
+++ core/src/space/hypeo/mankomania/actors/RectangleActor.java
@@ -0,0 +1,38 @@
+package space.hypeo.mankomania.actors;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * Created by pichlermarc on 26.04.2018.
+ */
+public class RectangleActor extends Actor {
+    ShapeRenderer rectangleRenderer;
+
+    public RectangleActor(float x, float y, float width, float height)
+    {
+        //Set up rectangle.
+        rectangleRenderer = new ShapeRenderer();
+        setX(x);
+        setY(y);
+        setWidth(width);
+        setHeight(height);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha)
+    {
+        batch.end();
+
+        rectangleRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        rectangleRenderer.setProjectionMatrix(batch.getProjectionMatrix());
+        rectangleRenderer.setTransformMatrix(batch.getTransformMatrix());
+        rectangleRenderer.translate(getX(), getY(), 0);
+        rectangleRenderer.setColor(this.getColor());
+        rectangleRenderer.rect(getX(), getY(), getWidth(), getHeight());
+        rectangleRenderer.end();
+
+        batch.begin();
+    }
+}
--- core/src/space/hypeo/mankomania/stages/SplashStage.java
+++ core/src/space/hypeo/mankomania/stages/SplashStage.java
@@ -1,6 +1,5 @@
 package space.hypeo.mankomania.stages;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -8,7 +7,10 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import org.w3c.dom.css.Rect;
+
 import space.hypeo.mankomania.StageManager;
+import space.hypeo.mankomania.actors.RectangleActor;
 
 /**
  * Created by pichlermarc on 25.04.2018.
@@ -21,38 +23,30 @@ public class SplashStage extends Stage {
 
     StageManager stageManager;
 
-    public SplashStage(StageManager stageManager, Viewport viewport)
-    {
+    public SplashStage(StageManager stageManager, Viewport viewport) {
         super(viewport);
         this.stageManager = stageManager;
 
         // Create actors.
+        RectangleActor background = new RectangleActor(0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
         Image title = new Image(new Texture("common/mankomania_logo.png"));
 
-        // TODO: Quick and dirty, change this to use world/viewport size.
-        title.setWidth(title.getWidth()/2.5f);
-        title.setHeight(title.getHeight()/2.5f);
-        title.setX((viewport.getWorldWidth()/2) - (title.getWidth()/2f));
-        title.setY((viewport.getWorldHeight()*2f/3f) - (title.getHeight()/2f));
-        // Add actors.
-        this.addActor(title);
+        title.setWidth(title.getWidth() / 2.5f);
+        title.setHeight(title.getHeight() / 2.5f);
+        title.setX((viewport.getWorldWidth() / 2) - (title.getWidth() / 2f));
+        title.setY((viewport.getWorldHeight() * 2f / 3f) - (title.getHeight() / 2f));
 
-        // TODO: figure out whether this is an ugly hack or not...
-        SplashStage stage = this;
+        background.setColor(237f/255f, 30f/255f, 121f/255f, 1f);
+
+        // Add actors.
+        this.addActor(background);
+        this.addActor(title);
 
         // Add listener for click events.
         this.addListener(new ClickListener() {
-        public void clicked(InputEvent event, float x, float y) {
-            stageManager.remove(stage);
-        }
-    });
+            public void clicked(InputEvent event, float x, float y) {
+                stageManager.remove(SplashStage.this);
+            }
+        });
     }
-
-    @Override
-    public void draw()
-    {
-        //TODO: Draw background.
-        super.draw();
-    }
-
 }
