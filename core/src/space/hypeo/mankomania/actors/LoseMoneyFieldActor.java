bdd442772560b07d9b55f9ebaeb0928e0f553f70
Marc Pichler (pichlermarc) <marc.pichler@edu.htl-villach.at>
2018-05-09 21:36:19 +0200

    LoseMoneyFieldActor, move texture path to constant.

--- core/src/space/hypeo/mankomania/actors/LoseMoneyFieldActor.java
+++ core/src/space/hypeo/mankomania/actors/LoseMoneyFieldActor.java
@@ -5,9 +5,10 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class LoseMoneyFieldActor extends FieldActor {
     private static final float FIELD_SCALE = 30f;
+    private static final String TEXTURE_PATH = "lose.jpg";
 
     public LoseMoneyFieldActor(float x, float y, Texture texture, int price, Image fieldInfoImage) {
-        super(new Texture("lose.jpg"), x, y, FIELD_SCALE, FIELD_SCALE, texture, price, fieldInfoImage);
+        super(new Texture(TEXTURE_PATH), x, y, FIELD_SCALE, FIELD_SCALE, texture, price, fieldInfoImage);
     }
 
     @Override
