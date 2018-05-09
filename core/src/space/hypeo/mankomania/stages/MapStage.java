1dc5fdcd8c66d2e7188c2c3c978ce82524e3d9f1
Marc Pichler (pichlermarc) <marc.pichler@edu.htl-villach.at>
2018-05-09 20:24:56 +0200

    extracted MapStage.

---
+++ core/src/space/hypeo/mankomania/stages/MapStage.java
@@ -0,0 +1,9 @@
+package space.hypeo.mankomania.stages;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+/**
+ * Created by pichlermarc on 09.05.2018.
+ */
+public class MapStage extends Stage {
+}
