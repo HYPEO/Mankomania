b0c089acfb4b7e43388126d12619e5a9af65b484
Marc Pichler (pichlermarc) <marc.pichler@edu.htl-villach.at>
2018-05-10 13:34:17 +0200

    downgraded gradle version, since tests would not work with android studio, only in cli.

---
+++ core/test/space/hypeo/mankomania/StageManagerTest.java
@@ -0,0 +1,25 @@
+package space.hypeo.mankomania;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pichlermarc on 10.05.2018.
+ */
+public class StageManagerTest {
+
+    @Test
+    public void push() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void getCurrentStage() {
+        assertFalse(false);
+    }
+
+    @Test
+    public void remove() {
+    }
+}
