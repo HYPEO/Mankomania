3792f1d2adcac6795366dfe2dba2afd4f77d5e34
Marc Pichler (pichlermarc) <marc.pichler@edu.htl-villach.at>
2018-05-10 14:17:57 +0200

    added sample unit test.

--- core/test/space/hypeo/mankomania/StageManagerTest.java
+++ core/test/space/hypeo/mankomania/StageManagerTest.java
@@ -1,25 +1,64 @@
 package space.hypeo.mankomania;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.EmptyStackException;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by pichlermarc on 10.05.2018.
  */
-public class StageManagerTest {
+public class StageManagerTest extends GameTest {
+    StageManager stageManager;
 
-    @Test
-    public void push() {
-        assertTrue(true);
+    @Mock
+    Stage firstStage;
+    @Mock
+    Stage secondStage;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Before
+    public void setUp() {
+        stageManager = new StageManager();
     }
 
     @Test
-    public void getCurrentStage() {
-        assertFalse(false);
+    public void push() {
+        stageManager.push(firstStage);
+        assertEquals(stageManager.getCurrentStage(), firstStage);
     }
 
     @Test
     public void remove() {
+        stageManager.push(firstStage);
+        stageManager.push(secondStage);
+        assertTrue(stageManager.remove(secondStage));
+        assertEquals(stageManager.getCurrentStage(), firstStage);
+
+    }
+
+    public void removeOnEmptyManager() {
+        try {
+            stageManager.remove(firstStage);
+            fail();
+        } catch (EmptyStackException exception) {
+        }
+    }
+
+    @Test
+    public void removeNonTopStage() {
+        stageManager.push(firstStage);
+        assertFalse(stageManager.remove(secondStage));
+        assertEquals(stageManager.getCurrentStage(), firstStage);
     }
 }
