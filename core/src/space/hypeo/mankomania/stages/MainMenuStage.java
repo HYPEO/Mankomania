7e5eaec878173040def718f33e7ca3f7f1b54eb9
Marc Pichler (pichlermarc) <marc.pichler@edu.htl-villach.at>
2018-05-11 20:15:50 +0200

    change main-menu to look nicer

--- core/src/space/hypeo/mankomania/stages/MainMenuStage.java
+++ core/src/space/hypeo/mankomania/stages/MainMenuStage.java
@@ -1,14 +1,17 @@
 package space.hypeo.mankomania.stages;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.esotericsoftware.minlog.Log;
@@ -16,6 +19,7 @@ import com.esotericsoftware.minlog.Log;
 
 import space.hypeo.mankomania.StageFactory;
 import space.hypeo.mankomania.StageManager;
+import space.hypeo.mankomania.actors.common.RectangleActor;
 import space.hypeo.networking.network.Role;
 import space.hypeo.networking.network.WhatAmI;
 
@@ -27,7 +31,7 @@ public class MainMenuStage extends Stage {
     private Button launch;
     private Button host;
     private Button join;
-    private Label title;
+    private Image title;
     private Table layout;
     private final Viewport viewport;
 
@@ -37,25 +41,41 @@ public class MainMenuStage extends Stage {
         this.stageManager = stageManager;
         this.viewport = viewport;
 
+        setUpBackground();
         createWidgets();
         setupClickListeners();
         setupLayout();
 
+        this.addActor(title);
         this.addActor(layout);
     }
 
     private void createWidgets() {
-
-        // Set up skin
-        Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
-
         // Set up Title.
-        title = new Label("MANKOMANIA (insert logo here)", skin);
+        Texture titleTexture = new Texture("common/mankomania_logo_shadowed.png");
+        titleTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        title = new Image(titleTexture);
 
-        // Set up buttons.
-        launch = new TextButton("Launch Game (offline)", skin);
-        host = new TextButton("Host Game", skin);
-        join = new TextButton("Join Game", skin);
+        launch = createButton("menu_buttons/play_offline.png", "menu_buttons/play_offline_clicked.png");
+        join = createButton("menu_buttons/join_game.png", "menu_buttons/join_game_clicked.png");
+        host = createButton("menu_buttons/host_game.png", "menu_buttons/host_game_clicked.png");
+    }
+
+    private Button createButton(String upTexture, String downTexture)
+    {
+        Texture hostTextureUp = new Texture(Gdx.files.internal(upTexture));
+        hostTextureUp.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+
+        Texture hostTextureDown = new Texture(Gdx.files.internal(downTexture));
+        hostTextureDown.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+
+        TextureRegion hostTextureRegionUp = new TextureRegion(hostTextureUp);
+        TextureRegion hostTextureRegionDown = new TextureRegion(hostTextureDown);
+
+        TextureRegionDrawable hostTextureRegionDrawableUp = new TextureRegionDrawable(hostTextureRegionUp);
+        TextureRegionDrawable hostTextureRegionDrawableDown = new TextureRegionDrawable(hostTextureRegionDown);
+
+        return new ImageButton(hostTextureRegionDrawableUp, hostTextureRegionDrawableDown);
     }
 
     private ClickListener launchClickListener() {
@@ -106,18 +126,33 @@ public class MainMenuStage extends Stage {
     }
 
     private void setupLayout() {
+        // Set up title.
+        title.setWidth(title.getWidth() / 2.5f);
+        title.setHeight(title.getHeight() / 2.5f);
+        title.setX((this.getViewport().getWorldWidth() / 2) - (title.getWidth() / 2f) + 10f);
+        title.setY((this.getViewport().getWorldHeight() * 3f / 4f) - (title.getHeight() / 2f));
+
         layout = new Table();
         layout.setWidth(this.getWidth());
         layout.align(Align.center);
-        layout.setPosition(0, this.getHeight() - 200);
+        layout.setPosition(0, this.getHeight() - 450);
         layout.padTop(50);
-        layout.add(title).width(300).height(100);
+        layout.add(title).width(400).height(100);
         layout.row();
-        layout.add(launch).width(300).height(100);
+        layout.add(launch).width(400).height(125);
         layout.row();
-        layout.add(host).width(300).height(100);
+        layout.add(host).width(400).height(125);
         layout.row();
-        layout.add(join).width(300).height(100);
+        layout.add(join).width(400).height(125);
         layout.row();
+    }
+
+    private void setUpBackground()
+    {
+        RectangleActor background = new RectangleActor(0, 0, this.getViewport().getWorldWidth(), this.getViewport().getWorldHeight());
+        // Set up background.
+        background.setColor(237f / 255f, 30f / 255f, 121f / 255f, 1f);
+
+        this.addActor(background);
     }
 }
