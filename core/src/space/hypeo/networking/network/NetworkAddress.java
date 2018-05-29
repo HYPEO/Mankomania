0cbc16d07781a6185cf1e2af2819cbf3d2d1943f
Christian Motz <christian.motz@gmx.at>
2018-05-29 22:41:23 +0200

    refactored network validate IP

--- core/src/space/hypeo/networking/network/NetworkAddress.java
+++ core/src/space/hypeo/networking/network/NetworkAddress.java
@@ -28,6 +28,20 @@ public final class NetworkAddress {
     public static final String IPADDRESS_PATTERN =
             "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
 
+    public static String validateIpAddress(String ipAddress) {
+        String validIpAddress = null;
+
+        Pattern pattern = Pattern.compile(IPADDRESS_PATTERN);
+
+        Matcher matcher = pattern.matcher(ipAddress);
+
+        if( matcher.find() ) {
+            validIpAddress = matcher.group();
+        };
+
+        return validIpAddress;
+    }
+
     /**
      * Gets all available network addresses for a device.
      * Filters loopback and inactive addresses out.
@@ -76,17 +90,11 @@ public final class NetworkAddress {
 
         String ip = "";
 
-        Pattern pattern = Pattern.compile(IPADDRESS_PATTERN);
-
         try {
             List<InetAddress> ipAddresses = NetworkAddress.getAllAvailableNetworkAddresses();
 
             for( InetAddress addr : ipAddresses ) {
-                Matcher matcher = pattern.matcher(addr.getHostAddress());
-
-                if( matcher.find() ) {
-                    ip = matcher.group();
-                }
+                ip = validateIpAddress(addr.getHostAddress());
             }
 
         } catch( SocketException e ) {
