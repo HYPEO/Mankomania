8e80ecba7cb5b3371c172e947cbeb78bf5b4cec2
Chrissi <chbergner@edu.aau.at>
2018-06-23 22:06:44 +0200

    predicted smells

--- core/src/space/hypeo/mankomania/stages/ClickerStageEndscreen.java
+++ core/src/space/hypeo/mankomania/stages/ClickerStageEndscreen.java
@@ -10,8 +10,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.esotericsoftware.minlog.Log;
-import space.hypeo.mankomania.StageFactory;
 import space.hypeo.mankomania.StageManager;
 import space.hypeo.mankomania.actors.common.RectangleActor;
 import space.hypeo.mankomania.actors.player.PlayerActor;
@@ -21,11 +19,6 @@ public class ClickerStageEndscreen extends Stage {
 
     private StageManager stageManager;
     private PlayerActor playerActor;
-    private Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
-    private TextButton buttonTrue;
-    private Table table = new Table();
-    private Label nameLabel;
-    private Label moneyLabel;
     private EconomicStageLogic eco;
     private int score;
 
@@ -49,16 +42,22 @@ public class ClickerStageEndscreen extends Stage {
     }
 
     private void setUpElements() {
+       Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
 
+        Table table = new Table();
         table.setWidth(this.getWidth());
         table.align(Align.bottom);
         table.setPosition(0, 200);
 
+
+        Label nameLabel;
+        Label moneyLabel;
         nameLabel = new Label("Du hast "+score +" ausgegeben", skin);
         nameLabel.setFontScale((float) 1.5);
         table.add(nameLabel).width(300).height(100).align(Align.center);
         table.row();
 
+        TextButton buttonTrue;
         buttonTrue = new TextButton("Okay", skin);
         buttonTrue.addListener(buttonClick());
         table.add(buttonTrue).width(100);
